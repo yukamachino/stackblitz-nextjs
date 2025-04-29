9da787e6c5ee676bed8f24b6b4f41b7f896cf0b4
yukamachino <yumachino.em@gmail.com>
2025-04-29 18:33:31 +0900

    Metadata Implementation

--- app/services/[category]/page.tsx
+++ app/services/[category]/page.tsx
@@ -1,13 +1,4 @@
-import { notFound } from "next/navigation";
-import CategoryPage from "./CategoryPage";
-import { generateAllParams, isImageRequest } from "@/utils/staticPageHelpers";
-
-import {
-  services,
-  isValidServiceCategory,
-} from "@/data/services";
-
-// 静的生成設定import { Metadata } from "next";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import CategoryPage from "./CategoryPage";
 import { generateAllParams, isImageRequest } from "@/utils/staticPageHelpers";
@@ -91,42 +82,6 @@ export async function generateMetadata({
     SITE_NAME
   );
 }
-
-// ページコンポーネント
-export default async function Page({ params }: PageProps) {
-  const resolvedParams = await params;
-  const category = resolvedParams.category;
-
-  // 画像リクエストの場合は何も返さない
-  if (isImageRequest(category)) {
-    return null;
-  }
-
-  // 有効なカテゴリかチェック
-  if (!isValidServiceCategory(category)) {
-    notFound();
-  }
-
-  return <CategoryPage category={category} />;
-}
-
-
-export const dynamic = "force-static";
-export const revalidate = false;
-
-// 静的生成のためのパスを生成
-export async function generateStaticParams() {
-  return generateAllParams({
-    categories: Object.keys(services),
-    imagePatterns: [".jpg"],
-    specialFiles: ["logo.svg"],
-  });
-}
-
-// パラメータの型定義
-type PageProps = {
-  params: Promise<{ category: string }>;
-};
 
 // ページコンポーネント
 export default async function Page({ params }: PageProps) {
