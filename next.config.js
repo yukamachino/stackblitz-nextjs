c18d0da4dd047680c48f2285e84f07e7e4513c3f
yukamachino <yumachino.em@gmail.com>
2025-04-29 18:12:36 +0900

    Implementing next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,23 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+// NODE_ENV は、開発中であれば "development"、本番環境なら "production" が設定
+// isProd には本番環境の場合 true、それ以外の場合は false が入る
+const isProd = process.env.NODE_ENV === "production";
 
-// module.exports = nextConfig;
+// ローカルプレビュー用変数 - .env.local で LOCAL_PREVIEW=true にするとbasePath と assetPrefix が適用されなくなる
+const isLocalPreview = process.env.LOCAL_PREVIEW === "true";
 
-/** @type {import('next').NextConfig} */
+// 静的生成用の設定
 const nextConfig = {
-    output: "export",
-    images: {
-      // Next.js の画像最適化機能を無効。静的エクスポートを行う場合、画像最適化は使用できないため
-      unoptimized: true,
-    },
-  
-    // 404エラーを修正するためのカスタム設定
-    trailingSlash: true,
+  output: "export",
+  // ローカルプレビュー時はベースパスを空にする
+  basePath: isProd && !isLocalPreview ? "/ご自身のプロジェクト名" : "",
+  assetPrefix: isProd && !isLocalPreview ? "/ご自身のプロジェクト名/" : "",
+  images: {
+    // Next.js の画像最適化機能を無効。静的エクスポートを行う場合、画像最適化は使用できないため
+    unoptimized: true,
+  },
+
+  // 404エラーを修正するためのカスタム設定
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
